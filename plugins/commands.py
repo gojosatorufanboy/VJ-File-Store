d4cc1f324aeca9833318f25ccf4a27f96defbc66
Gojo <101966680+gojosatorufanboy@users.noreply.github.com>
2024-03-12 12:40:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -227,7 +227,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                      [
-                         InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback.data=f'generate_stream_link:{file_id}')
+                         InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}')
                      ]
                     ]
                 )
